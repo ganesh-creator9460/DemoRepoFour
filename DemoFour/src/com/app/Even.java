0af0e35f4ded0b22e81def20b6b1848aef3b845d
ganes <ganes@DESKTOP-4CMOULL>
2025-09-17 14:53:08 +0530

    Even number code

---
+++ DemoFour/src/com/app/Even.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Even {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
